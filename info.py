3450f0939c9f9d51b3d8b6a1e426d04d2607bad5
Mrwitcher05 <cinemacityhub@gmail.com>
2025-01-05 12:58:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -150,9 +150,9 @@ QUALITIES = ["360p", "480p", "720p", "1080p", "1440p", "2160p"]
 YEARS = ["1900", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024", "2025"]
 
 
-                           # Don't Remove Credit @MrWitcherTN
-                   
-                           # Ask Doubt on telegram @TNMCLink
+                           # Don't Remove Credit @VJ_Botz
+                           # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+                           # Ask Doubt on telegram @KingVJ01
 
 
 # Online Stream and Download
