ab76a30c04394a1581dcdb39b9901bff97c6742a
Mrwitcher05 <cinemacityhub@gmail.com>
2025-01-10 08:24:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -125,7 +125,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False)) # Set True Or False
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
 SHORTLINK_URL = environ.get('modijiurl.com', '')
 SHORTLINK_API = environ.get('7093f846c0cb92afe2a6b65b7581f835f7a39b21', '')
 TUTORIAL = environ.get('https://t.me/Dreams_Office/3', '') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
