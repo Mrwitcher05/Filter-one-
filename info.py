53eb489c4555354f471be0a23769053064925a83
Tamilvi <Tamilcartoonvi@gmail.com>
2025-01-11 21:22:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -59,8 +59,8 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://mani:mani09@raani.pqqj1.mongodb.net/?retryWrites=true&w=majority&appName=raani")   # IF Multiple Database Is False Then Fill Only This Database Url.
-DATABASE_NAME = environ.get('DATABASE_NAME', "mani")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://tamilcartoon:tamilcartoon@kavin.uwvt7.mongodb.net/?retryWrites=true&w=majority&appName=kavin")   # IF Multiple Database Is False Then Fill Only This Database Url.
+DATABASE_NAME = environ.get('DATABASE_NAME', "tamilcartoon")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'vjcollection')
 
 MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False)) # Set True or False
